3b2dc33e94fcf50844ac389dc908359c8c232f25
Mohamed Elshadid <elshadid_mohamed@yahoo.com>
2020-08-08 16:30:44 +0200

    Stock Component

---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="col-sm-6 col-md-4">
+        <div class="card mt-4">
+            <div class="card-body">
+                <h4 class="card-title text-center alert-success">
+                    NAME
+                    <small>(Price: PRICE)</small></h4>
+                    <div>
+                        <input 
+                            type="number"
+                            class="form-control"
+                            placeholder="Quantity"
+                            >
+                    </div>
+                    <div class="text-right mt-2">
+                        <button class="btn btn-success">Buy</button>
+                    </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,10 +1,26 @@
 <template>
-  <h1>s</h1>
+  <div class="row">
+      <app-stock v-for="stock in stocks" :key="stock.id"></app-stock>
+  </div>
 </template>
 
 <script>
+import Stock from './Stock';
 export default {
-
+    components:{
+        appStock:Stock
+    },
+    data(){
+        return {
+            stocks:[
+                {id:1,name:"BMW",price:110},
+                {id:2,name:"Google",price:150},
+                {id:3,name:"Facebook",price:130},
+                {id:4,name:"Apple",price:200},
+                {id:5,name:"Twitter",price:80}
+            ]
+        }
+    }
 }
 </script>
 
